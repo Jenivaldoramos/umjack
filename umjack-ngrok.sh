bdd66307bee9c02caa619a4cd8135b0e05866f2d
Jenivaldoramos <93117561+Jenivaldoramos@users.noreply.github.com>
2022-04-17 15:22:36 -0300

    Add files via upload

---
+++ umjack-ngrok.sh
@@ -0,0 +1,50 @@
+#!/bin/dash
+atualizar='\033[1;32m'
+echo "$atualizar Atualizando repositório..."
+apt update && upgrade -y
+apt install -y wget
+clear
+k='\033[01;32m'
+echo
+echo "${k}████████╗ ██████╗██╗  ██╗███████╗██╗      ██████╗ ";
+echo "${k}╚══██╔══╝██╔════╝██║  ██║██╔════╝██║     ██╔═══██╗";
+echo "${k}   ██║   ██║     ███████║█████╗  ██║     ██║   ██║";
+echo "${k}   ██║   ██║     ██╔══██║██╔══╝  ██║     ██║   ██║";
+echo "${k}   ██║   ╚██████╗██║  ██║███████╗███████╗╚██████╔╝";
+echo "${k}   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ";
+echo "${k} /_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/";
+echo
+echo "desenvolvido por jenivaldo ramos"
+echo
+echo "Deseja instalar o Ngrok? [Y/n]"
+read opcao
+case $opcao in
+y)
+echo
+echo "Downloading Termux-ngrok..."
+
+
+wget "https://github.com/tchelospy/NgrokTest"
+unzip NgrokTest/ngrok-stable-linux-arm.zip
+cp NgrokTest/ngrok /usr/bin/ngrok
+chmod 700 /usr/bin/ngrok
+
+clear
+echo "${k}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
+echo "${k}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
+echo "${k}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
+echo "${k}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
+echo "${k}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
+echo "${k}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
+echo
+echo "Exemplo de comando (./ngrok http 80) | Ou (ngrok --help)"
+;;
+
+n)
+clear
+echo "Ngrok não instalado :("
+echo
+esac
+
+
+
